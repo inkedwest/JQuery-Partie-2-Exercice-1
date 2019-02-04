2ac9fb3f1f2148adf6150d2dec732d547600f228
Glatigny <f.west@live.fr>
2019-02-04 11:53:57 +0100

    exo 1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(function(){ // On appelle la fonction
+    $(document).ready(function(){ // On relie le HTML
+        $('#clickMe').click(function(){ // Fonction au clic
+            alert('Mes salutations preu chevalier !'); // Boîte de dialogue avec message à afficher
+        });
+    });
+});
